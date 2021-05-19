7b1a8dd33a679ccaae9e5a5f4049d355ece6d456
Cristian López <andresgaete04@gmail.com>
2021-05-18 21:51:11 -0400

    docs agregado

---
+++ docs/precache-manifest.589d91c6dbd118f76bca34c0c56e9ff7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e2483d565199eeba476cefdb96f7a7f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7cc2596b2f6cb252ef1d",
+    "url": "/static/css/main.87b4c4a9.chunk.css"
+  },
+  {
+    "revision": "948005f112a70a9ef03d",
+    "url": "/static/js/2.1aebb2d0.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.1aebb2d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cc2596b2f6cb252ef1d",
+    "url": "/static/js/main.a6c5967b.chunk.js"
+  },
+  {
+    "revision": "4e3a1f73b6e5de40b557",
+    "url": "/static/js/runtime-main.5c054f69.js"
+  }
+]);
